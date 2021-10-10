794708f909c5cf91545c1a8a0c955c17259df959
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-10 15:11:59 +0530

    Implemented Save Button in Passenger Count App

--- 1.PassengerCountingApp/index.js
+++ 1.PassengerCountingApp/index.js
@@ -1,12 +1,16 @@
 
+let saveEl = document.getElementById('save-el')
 let countEl= document.getElementById('count-el');
-count=countEl.innerText
 
+count=countEl.innerText
 function increment () {
     count++
     countEl.innerText = count
 }
 
 function save() {
+    let saved = saveEl.innerText
+    saved+= " " + count
+    saveEl.innerText = saved
     console.log(count);
 }
