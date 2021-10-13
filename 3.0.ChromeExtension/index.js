a77be784f6dbd12aaf4f75369e4831dbc6b5fc05
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-13 20:29:39 +0530

    Learnt about creating element using js

--- 3.0.ChromeExtension/index.js
+++ 3.0.ChromeExtension/index.js
@@ -1,12 +1,22 @@
 
 // Array to store the data
-let myLeads = []
+let myLeads = ["a","b"]
 
 //Query Selectors
 const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
+const ulEl = document.getElementById('ul-el')
 
 //Event Listeners
 inputBtn.addEventListener("click", function(){
-    console.log("Button Clicked");
+    // Getting Value from input element directly
+    let lead = inputEl.value
+    myLeads.push(lead)
 })
+
+let ListItems=""
+for (let index = 0; index < myLeads.length; index++) {
+    ListItems+=`<li> ${myLeads[index]} </li>`
+}
+
+ulEl.innerHTML=ListItems
