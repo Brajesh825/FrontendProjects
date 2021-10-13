553928c396bf2d480d167147264a7f37222efbc4
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-13 19:53:41 +0530

    Learnt about addEventListener and Const Keywords

---
+++ 3.0.ChromeExtension/index.js
@@ -0,0 +1,12 @@
+
+// Array to store the data
+let myLeads = []
+
+//Query Selectors
+const inputBtn = document.getElementById('input-btn')
+const inputEl = document.getElementById('input-el')
+
+//Event Listeners
+inputBtn.addEventListener("click", function(){
+    console.log("Button Clicked");
+})
