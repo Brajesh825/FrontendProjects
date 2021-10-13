1bc74c9d18a4c75588b724911468df6edb1196bd
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-13 15:20:44 +0530

    Completed BlackJack Game

--- 2.0.BlackJack/index.js
+++ 2.0.BlackJack/index.js
@@ -8,17 +8,33 @@ let message = ""
 //Player Information
 let player ={
     name:"Brajesh",
-    chips:145
+    chips:100
 }
+player.name=prompt("Please Enter Your Name")
 
 //QuerySelectors
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
 let cardsEl = document.getElementById('cards-el')
 let playerEl = document.getElementById('player-el')
+let startEl = document.getElementById('start-el')
+let newCardEl = document.getElementById('new-card-el')
 
 //Assigning Player Values
-playerEl.textContent = `${player.name}: $${player.chips} `
+playerEl.textContent =  `${player.name}: $${player.chips} `
+
+function updatePlayer(){
+    if(hasBlackJack){
+        hasBlackJack=false
+        player.chips+=5
+    }else{
+        player.chips-=1
+    }
+    isAlive=false
+    playerEl.textContent =  `${player.name}: $${player.chips} `
+    startEl.style.backgroundColor = "goldenrod"
+    newCardEl.style.backgroundColor = "#ccc"
+}
 
 function getRandomCard(){
     let randomNumber= Math.floor(Math.random()*13)+1;
@@ -32,12 +48,16 @@ function getRandomCard(){
 }
 
 function startGame(){
-    isAlive=true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards =[firstCard,secondCard]
-    sum = firstCard + secondCard
-    renderGame()
+    if(!isAlive){
+        isAlive=true
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        cards =[firstCard,secondCard]
+        sum = firstCard + secondCard
+        startEl.style.backgroundColor = "#ccc"
+        newCardEl.style.backgroundColor = "goldenrod"
+        renderGame()
+    }
 }
 
 function newCard(){
@@ -61,9 +81,11 @@ function renderGame(){
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
+        updatePlayer()
     } else {
         message = "You're out of the game!"
         isAlive = false
+        updatePlayer()
     }
     messageEl.textContent= message
 } 
