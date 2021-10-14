7b3d3946475150ae51a6b72472a11046de954fdd
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-14 20:59:27 +0530

    Used LocalStorage to handle the Leads

--- 3.0.ChromeExtension/index.js
+++ 3.0.ChromeExtension/index.js
@@ -1,5 +1,4 @@
-
-// Array to store the data
+// Array To Store The Data
 let myLeads = []
 
 //Query Selectors
@@ -7,15 +6,23 @@ const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 
+//Local Storage Handle
+function populate() {
+    for (let index = 0; index < localStorage.length; index++) {
+        myLeads.push(localStorage.getItem(`myLeads:${index}`))
+    }    
+}
+
 //Event Listeners
 inputBtn.addEventListener("click", function(){
-    // Getting Value from input element directly
+// Getting Value from input element directly
     let lead = inputEl.value
     inputEl.value=""
     myLeads.push(lead)
     renderLeads()
 })
 
+// Render The Inputed Links
 function renderLeads(){
     let listItems=""
     for (let index = 0; index < myLeads.length; index++) {
@@ -23,8 +30,11 @@ function renderLeads(){
             `<li>
                 <a href="${myLeads[index]}" target="_blank"> ${myLeads[index]} </a>
             </li>`
+            localStorage.setItem(`myLeads:${index}`,myLeads[index])
     }
     ulEl.innerHTML=listItems
 }
 
-
+// Initialisers
+populate()
+renderLeads()
