4b67d0030f3bde13b72f2aff1ab03ab7ae0ddf33
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-15 20:47:31 +0530

    Added a tab button which fetch current url into localstorage using chrome api

--- 3.0.ChromeExtension/index.js
+++ 3.0.ChromeExtension/index.js
@@ -9,12 +9,14 @@ const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 const deleteAllBtn = document.getElementById('delete-all-btn')
+const tabBtn = document.getElementById('tab-btn')
 
 //Initialiser
 if(leadFromLocalStorage){
     myLeads=leadFromLocalStorage
     render(myLeads)
 }
+
 // Render The Inputed Leads
 function render(leads){
     let listItems=""
@@ -34,6 +36,15 @@ inputBtn.addEventListener("click", function(){
     localStorage.setItem("myleads",JSON.stringify(myLeads))
     render(myLeads)
 })
+
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active: true,curentWindow: true},function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myleads",JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
 deleteAllBtn.addEventListener("dblclick",function(){
     localStorage.clear()
     myLeads=[]
